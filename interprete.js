f39514d5c9467447f9f0b3df97d37bdb6985d2c3
Serra <119073169+Cosserra@users.noreply.github.com>
2025-05-08 16:30:55 +0200

    Ejemplos mejorados

--- interprete.js
+++ interprete.js
@@ -6,6 +6,13 @@ buttonEjecutar.addEventListener("click", runXPath);
 buttonEjemplo.addEventListener("click", restoreExampleXML);
 xpathQuery.addEventListener("input", runXPath);
 
+const copiables = document.querySelectorAll(".copiable");
+copiables.forEach(element => {
+  element.addEventListener("click", () => {
+    copyToClipboard(element.textContent);
+  });
+});
+
 function runXPath() {
     const xmlStr = document.getElementById('xml').value;
     const xpath = document.getElementById('xpath').value;
@@ -43,29 +50,97 @@ function runXPath() {
 }
 
 function restoreExampleXML() {
-    const exampleXML = `<catalog>
+    const exampleXML = 
+`<catalog>
+  <section name="Books">
     <book id="bk101" category="fiction">
       <title>Harry Potter</title>
-      <author>J.K. Rowling</author>
-      <price>29.99</price>
-      <description>Once upon a time...</description>
+      <authors>
+        <author nationality="UK">J.K. Rowling</author>
+      </authors>
+      <price currency="USD">29.99</price>
+      <description>
+        <summary>Once upon a time...</summary>
+        <keywords>
+          <keyword>Magic</keyword>
+          <keyword>Wizards</keyword>
+        </keywords>
+      </description>
     </book>
     <book id="bk102" category="non-fiction">
       <title>Steve Jobs</title>
-      <author>Walter Isaacson</author>
-      <price>24.95</price>
-      <description>Biography of Steve Jobs.</description>
+      <authors>
+        <author nationality="US">Walter Isaacson</author>
+      </authors>
+      <price currency="USD">24.95</price>
+      <description>
+        <summary>Biography of Steve Jobs.</summary>
+        <keywords>
+          <keyword>Technology</keyword>
+          <keyword>Apple</keyword>
+        </keywords>
+      </description>
     </book>
     <book id="bk103" category="fiction">
       <title>The Hobbit</title>
-      <author>J.R.R. Tolkien</author>
-      <price>22.99</price>
-      <description>An unexpected journey.</description>
+      <authors>
+        <author nationality="UK">J.R.R. Tolkien</author>
+      </authors>
+      <price currency="USD">22.99</price>
+      <description>
+        <summary>An unexpected journey.</summary>
+      </description>
     </book>
+    <book id="bk104" category="non-fiction">
+      <title>¿Y si fuéramos nosotros?</title>
+      <authors>
+        <author nationality="US">Becky Albertalli</author>
+        <author nationality="US">Adam Silvera</author>
+      </authors>
+      <price currency="EUR" status="sale">15.20</price>
+      <description>
+        <summary>Arthur is alone in New York.</summary>
+      </description>
+    </book>
+  </section>
+
+  <section name="Magazines">
     <magazine id="mg201" category="science">
       <title>National Geographic</title>
-      <issue>2023-09</issue>
+      <issue>
+        <date>2023-09</date>
+        <editor>Jane Doe</editor>
+      </issue>
     </magazine>
-  </catalog>`;
+    <magazine id="mg202" category="history">
+      <title>Historia National</title>
+      <issue>
+        <date>2024-01</date>
+        <editor nationality="ES">Carlos Ruiz</editor>
+      </issue>
+    </magazine>
+  </section>
+</catalog>`;
     document.getElementById('xml').value = exampleXML;
+}
+
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text).then(() => {
+    Swal.fire({
+      icon: 'success',
+      title: 'Texto copiado',
+      text: text,
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true
+    });
+  }).catch(err => {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error al copiar',
+      text: err,
+    });
+  });
 }
